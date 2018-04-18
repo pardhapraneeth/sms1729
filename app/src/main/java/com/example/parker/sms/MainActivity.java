b84c05de5a0a3b4f28c0789f062e47fece59c452
PUDI PARDHA PRANEETH <cse15u030@OCTA.EDU>
2018-04-18 15:57:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parker/sms/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.parker.sms;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+public class MainActivity extends Activity {
+
+    EditText mobileno,message;
+    Button sendsms;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mobileno=(EditText)findViewById(R.id.editText);
+        message=(EditText)findViewById(R.id.editText2);
+        sendsms=(Button)findViewById(R.id.button);
+
+        //Performing action on button click
+        sendsms.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                String no=mobileno.getText().toString();
+                String msg=message.getText().toString();
+
+                //Getting intent and PendingIntent instance
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+
+                //Get the SmsManager instance and call the sendTextMessage method to send message
+                SmsManager sms=SmsManager.getDefault();
+                sms.sendTextMessage(no, null, msg, pi,null);
+
+                Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+}
